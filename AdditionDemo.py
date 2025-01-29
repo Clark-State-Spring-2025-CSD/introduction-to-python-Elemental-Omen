b7d0ef739db44fe1195a46b6290ec948ac4e065b
Micah <micah.echols.11@icloud.com>
2025-01-28 21:30:45 -0500

    Work for addition demo and average assignment

---
+++ AdditionDemo.py
@@ -0,0 +1,5 @@
+x = 8
+y = 10
+z = x + y
+Answer = f"the sum of {x} and {y} is {z}"
+print(Answer)
---
+++ AverageAssignment.py
@@ -0,0 +1,5 @@
+x=4
+y=2
+z=12
+avarage=(x+y+z)/3
+print(f"The average of {x}, {y} and {z} is {avarage}")  
